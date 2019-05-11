12381a0cb748d8e49877677daf3e871a5cab3138
marcosvandrade <vascorj2006@gmail.com>
2019-05-11 18:46:28 -0300

    CommitSeminarioSamba

--- index.php
+++ index.php
@@ -2,48 +2,73 @@
 <html lang="en">
   <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="style.css">
 
     <title>Seminário de Linux - Samba</title>
   </head>
   <body>
     <!--navbar-->
-    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
-        <div class="container">
-            
-            <a class="navbar-brand ml-auto" ref="#">Seminário de Linux - Samba</a>
-            
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #563D7C">
+              
+                <a class="navbar-brand ml-auto" href="#" id="nav">Seminário de Linux - Samba</a>
                 
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav ml-auto">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Apresentação
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="slide1.html">Slide 1</a>
-                    <a class="dropdown-item" href="#">Slide 2</a>
-                    <a class="dropdown-item" href="#">Slide 3</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                    
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Apresentação
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="slide1.html" target="_blank">Slide 1</a>
+                            <a class="dropdown-item" href="#" target="_blank">Slide 2</a>
+                            <a class="dropdown-item" href="#" target="_blank">Slide 3</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+        </nav>
+     </div>
+	
+            <div class="container">
+                <div class="row">
+                    <div id="texto" class="col" >
+                        Grupo: Marcos Vasconcellos de Andrade e Luiz Mir
                     </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-</nav>
+                    <div class="w-100"></div>
+                    <div id="texto" class="col">
+                        Professor: Diógenes
+                    </div>
+                    <div class="w-100"></div>
+                    <div id="texto" class="col">
+                        Curso: TSI3/Noturno
+                    </div>
 
-		<!-- conteudo dos slides -->
-    	<div>
-				 
-    			
-		</div>
-       
+                </div>
+            </div>
+            
+            <div class="container">
+                <h3>Introdução</h3>                
+            </div>
+
+            <div class="container">
+                <p class="text-left">- Breve Histórico</p>
+                <p class="text-left">- Funcionalidades do SAMBA</p>
+                <p class="text-left">- Instalaçao e Configuração</p>
+                <p class="text-left">- LABORATÓRIO</p>
+
+            </div>		 
+        		
         
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
