7351ed67ee9c932fffd0992d3eee01287dc9fd33
marcosvandrade <vascorj2006@gmail.com>
2019-06-09 13:48:19 -0300

    commitFinal

--- index.php
+++ index.php
@@ -15,52 +15,19 @@
   <body>
     <!--navbar-->
     <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #563D7C">
-              
-                <a class="navbar-brand ml-auto" href="#" id="nav">Seminário de Linux</a>
-                
-               <img src="images/samba.png" class="img-fluid" alt="Responsive image" />
-
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        
+        <nav class="navbar" id="nav" style="background-color: #563D7C">Seminário de Linux 
                     
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Apresentação
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="slides/slide1.html" target="_blank">Slide 1</a>
-                            <a class="dropdown-item" href="slides/slide2.html" target="_blank">Slide 2</a>
-                            <a class="dropdown-item" href="slides/slide3.html" target="_blank">Slide 3</a>
-                            <a class="dropdown-item" href="slides/slide4.html" target="_blank">Slide 4</a>
-                            <a class="dropdown-item" href="slides/slide5.html" target="_blank">Slide 5</a>
-                            <a class="dropdown-item" href="slides/slide6.html" target="_blank">Slide 6</a>
-                            <a class="dropdown-item" href="slides/slide7.html" target="_blank">Slide 7</a>
-                            <a class="dropdown-item" href="slides/slide8.html" target="_blank">Slide 8</a>
-                            <a class="dropdown-item" href="slides/slide9.html" target="_blank">Slide 9</a>
-                            <a class="dropdown-item" href="slides/slide10.html" target="_blank">Slide 10</a>
-                            <a class="dropdown-item" href="slides/slide11.html" target="_blank">Slide 11</a>
-                            <a class="dropdown-item" href="slides/slide12.html" target="_blank">Slide 12</a>
-                            <a class="dropdown-item" href="slides/slide13.html" target="_blank">Slide 13</a>
-                            <a class="dropdown-item" href="slides/slide14.html" target="_blank">Slide 14</a>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
+                <img src="images/samba.png" class="rounded float-right" alt="Responsive image" /> 
+            
         </nav>
+     
      </div>
 	
             <div class="container">
                 <div class="row">
                     <div id="texto" class="col" >
-                        Grupo: Marcos Vasconcellos de Andrade</br>
-                               &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-                               Luiz Jorge Mir Filho</br>
-                               &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-                               Edilson Nery Barbosa
+                        Grupo: Marcos Vasconcellos de Andrade
                     </div>
                     <div class="w-100"></div>
                     <div id="texto" class="col">
@@ -85,14 +52,45 @@
                 <p class="text-left">- Funcionalidades do SAMBA</p>
                 <p class="text-left">- Instalaçao e Configuração</p>
                 <p class="text-left">- Ajustes Finais</p>
-                <p class="text-left">- LABORATÓRIO</p>
+                <p class="text-left">- LABORATÓRIO</p><br><br>
 
             </div>		 
-        	
+                      
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
+
+  <footer>
+    <div class="container">
+        <nav aria-label="...">
+            <ul class="pagination justify-content-center">
+                
+                <li class="page-item disabled"><a href="#" aria-label="anterior"><span aria-hidden="true"></span></a></li>
+                
+                <li class="page-item active"><a class="page-link" href="index.php">1</a></li>
+                <li class="page-item"><a class="page-link" href="slides/slide1.html">2</a></li>
+                <li class="page-item"><a class="page-link" href="slides/slide2.html">3</a></li>
+                <li class="page-item"><a class="page-link" href="slides/slide3.html">4</a></li>
+                <li class="page-item"><a class="page-link" href="slides/slide4.html">5</a></li>
+                <li class="page-item"><a class="page-link" href="slides/slide5.html">6</a></li>
+                <li class="page-item"><a class="page-link" href="slides/slide6.html">7</a></li>
+                <li class="page-item"><a class="page-link" href="slides/slide7.html">8</a></li>
+                <li class="page-item"><a class="page-link" href="slides/slide8.html">9</a></li>
+                <li class="page-item"><a class="page-link" href="slides/slide9.html">10</a></li>
+                <li class="page-item"><a class="page-link" href="slides/slide10.html">11</a></li>
+                <li class="page-item"><a class="page-link" href="slides/slide11.html">12</a></li>
+                <li class="page-item"><a class="page-link" href="slides/slide12.html">13</a></li>
+                <li class="page-item"><a class="page-link" href="slides/slide13.html">14</a></li>
+                <li class="page-item"><a class="page-link" href="slides/slide14.html">15</a></li>
+                                               
+                <li><a href="#" aria-label="Próximo"><span aria-hidden="true"></span></a></li>
+            </ul>
+        </nav>
+    </div>
+  </footer>
+
+
 </html>
